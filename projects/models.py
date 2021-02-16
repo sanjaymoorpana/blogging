23edcdd75b142c8bda372c26fe244da01ea19b78
sanjaymoorpana <sjmoorpana@gmail.com>
2021-02-16 17:29:31 +0530

    first commit

---
+++ projects/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Project(models.Model):
+
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    technology = models.CharField(max_length=100)
+    images = models.ImageField()
+    
+    def __str__(self):
+        return self.title
